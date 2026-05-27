8cbb911733c7710de40f7cb59d191de551d7d70f
HenriqueMartinsAzevedo <devqraazevedo@gmail.com>
2026-05-27 11:34:03 -0300

    Resolvedo a questão de inicializar a árvore vazia e inserir nó filho

--- lista_de_exercicios_01/arvore_generica.C
+++ lista_de_exercicios_01/arvore_generica.C
@@ -12,10 +12,22 @@
 // 11 - O ancestral é nó que está acima de outro nó na árvore e os descendentes são os nós que está abaixo de outro nó na árvore.
 // 12 - A profundidade é a quantidade de ligações no caminho da raiz até o nó que está sendo buscado.
 // 13 - O grau de um nó é o número de filhos do nó. O grau de uma árvore é o máximo de grau dos nós.
-// 14 - As árvores são uma estrutura de dados importante dado ao fato que 
+// 14 - As árvores são estruturas de dados importantes devido ao fato de que, em certas situações, as buscas são mais rápidas do que em listas. A organização dos dados de forma hierárica também facilita buscas posteriores.
+// 15 - 
+//             10  
+//      /       |     \
+//     5       14      20
+//    / \     /  \       \
+//   3   7   12  16       27
+//  /
+// 1
+
+// Folha: 1, 7, 12, 16, 27.
+// Altura: Nível 3.
+// Grau da árvore: 3.
 #include <stdlib.h>
 #include <stdio.h>
-
+//16
 //--------------------------
 //Estrutura do nó
 //--------------------------
@@ -32,6 +44,25 @@ No* CriarNO(int valor){
     novo->prim_filho = NULL;
     novo->prox_irmao = NULL;
 
+    return novo;
+}
+
+int inserir(No *pai, No *filho){
+    //17 - Inicializando uma árvore vazia.
+    if(pai->prim_filho == NULL){
+        pai->prim_filho = filho;
+    }
+
+    //18 - Inserindo um filho no nó.
+    No *aux = pai->prim_filho; // A VAR aux é para não perder a referência do primeiro irmão.
+
+    while(aux->prox_irmao != NULL){
+        aux = aux->prox_irmao;
+    }
+
+    aux->prox_irmao = filho;
+
+    return 1;
 
 }
 int main(){
