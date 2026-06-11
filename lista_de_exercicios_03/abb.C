6613a0557142e1eb5dff39a356f01d9b652e0014
HenriqueMartinsAzevedo <devqraazevedo@gmail.com>
2026-06-11 18:26:26 -0300

    Respostas questão conceitual 9

--- lista_de_exercicios_03/abb.C
+++ lista_de_exercicios_03/abb.C
@@ -9,4 +9,4 @@
 // 7 - Remoção de nó com um filho: Neste caso, o filho é colocado no lugar no pai (Nó a ser removido), depois que é feito a ligação, o nó é removido.
 //7 - Remoção do nó com dois filhos: Já neste caso, podemos seguir a regra do sucessor que o nó a ser removido, vai ser substituído pelo nó de menor valor da subárvore da direita, ou podemos seguir a regra do predecessor que é o nó de maior valor da subárvore da esquerda.
 //8 - A principal vantagem é que a ABB possui regra de ordenação as dos valores menores que a raíz ficam a esquerda e valores maiores que a raíz ficada na direiita, consequentemente o processo de busca mais rápido.
-//9 - 
+//9 - O problema do desbalanceamento em ABB é que o tempo de busca será maior, por exemplo se o valor procurado for uma folha de 1000 nós a complexidade cai para O(n), já na ABB balanceada a complexidade é de O(log n) que é mais rápido.
