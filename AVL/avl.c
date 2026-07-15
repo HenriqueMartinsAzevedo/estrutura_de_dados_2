f327e54b892784538d7793c87fd0521a53928f47
HenriqueMartinsAzevedo <devqraazevedo@gmail.com>
2026-07-15 13:08:29 -0300

    criação da estrutura do no e da função criarNo

---
+++ AVL/avl.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct No{
+    int valor;
+    struct No* esq;
+    struct No* dir;
+    int altura;
+} No;
+
+//função para criar o no.
+//essa função cria o no e por fim retorna o endereço de memória do no.
+No* criarNo(int valor){
+    No* novo = (No*) malloc(sizeof(No));
+
+    if(novo == NULL) return NULL;
+
+    novo->valor = valor;
+    novo->esq = NULL;
+    novo->dir = NULL;
+    novo->altura = 0;
+    
+    return novo;
+}
+
